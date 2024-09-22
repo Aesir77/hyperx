796f9007d4eb8694501d7cdead7030aaeb1010c3
Aesir77 <134356658+Aesir77@users.noreply.github.com>
2024-09-22 15:53:19 +0800

    Co-authored-by: Joshua Gabriel L. Flores <Joshuaflores12@users.noreply.github.com> Co-authored-by: RexRegius2004 <RexRegius2004@users.noreply.github.com> Co-authored-by: marcutechxd070703@outlook.com <marcutechxd070703@outlook.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import *  as reactRouterDom from "react-router-dom";
+import Mynavbar from './Mynavbar';
+import Home from './component/Home.js';
+import Aboutus from './component/About-us.js';
+import LogIn from './component/Log-In.js';
+import Reviews from './component/Reviews.js';
+import Checkin from './component/Check-in.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (  
+    <reactRouterDom.BrowserRouter>
+      <Mynavbar/>
+    <reactRouterDom.Routes>
+      <reactRouterDom.Route path="/" element={<Home />} />
+      <reactRouterDom.Route path="/about" element={<Aboutus />} />
+      <reactRouterDom.Route path="/log-in" element={<LogIn />} />
+      <reactRouterDom.Route path="/reviews" element={<Reviews />} />
+      <reactRouterDom.Route path="check-in" element={<Checkin />} />
+
+    </reactRouterDom.Routes>  
+   </reactRouterDom.BrowserRouter>
   );
 }
 
---
+++ src/component/About-us.js
@@ -0,0 +1,19 @@
+
+function Aboutus () {
+return (
+    <div className="About-us">
+
+<h1> About Us </h1>
+
+
+
+
+
+
+
+    </div>
+);
+}
+
+export default Aboutus;
+
