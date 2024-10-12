522019c60aa86ad3e8d3cba6ff342b67fee5d584
Josh Guinto <jmpheen@gmail.com>
2024-10-12 20:43:06 +0800

    Added Booking

---
+++ src/component/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './styling/footer.css'
+function Footer() {
+return (
+
+<div className="footer">
+<p> Contact Number: +63997990439 </p>
+<p> Email: hyper.X@worlds.com </p>
+<p> Icons here </p>
+
+
+</div>
+);
+}
+
+export default Footer;
