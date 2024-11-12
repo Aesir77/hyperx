21d90dda99c2f724ba9b0461220372d998b122a9
Aesir77 <134356658+Aesir77@users.noreply.github.com>
2024-11-12 23:52:58 +0800

    test1

--- src/component/About-us.js
+++ src/component/About-us.js
@@ -1,37 +1,3 @@
-<<<<<<< Updated upstream
-import React from "react";
-
-function Aboutus() {
-  return (
-    <div className="About-us">
-      <h1>What We Are</h1>
-      <p>
-        We are HyperX, a moving hotel that travels from one galaxy to another, bringing you interstellar comfort across
-        the galaxies. The destinations are endless, offering experiences far beyond your homeworld's reach. 
-        With eye-catching stars as your view, rooms as comfortable as your home, and meals as exquisite as fine dining.
-        Tour far and wide across the reaches of galaxies, and may the force be with you.
-      </p>
-    </div>
-  );
-}
-
-export default Aboutus;
-=======
-<<<<<<< Updated upstream
-
-function Aboutus () {
-return (
-    <div className="About-us">
-
-<h1> About Us </h1>
-
-
-
-
-
-
-
-=======
 import React from "react";
 import './styling/about.css';
 
@@ -62,11 +28,11 @@ function Aboutus() {
 
 
        </p>
->>>>>>> Stashed changes
+
     </div>
 );
 }
 
 export default Aboutus;
 
->>>>>>> Stashed changes
+
